70044a6cf440a3c907447b099e4ce9028df2a195
Jonas Sørensen <jonas7793@gmail.com>
2020-04-03 22:49:54 +0200

    test

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
     define("IN_APP", true);
     ob_start();
     session_start();
+    echo "eh";
 
     //require_once "controller/class.Configuration.php";
     //$Configuration = Configuration::getInstance();
